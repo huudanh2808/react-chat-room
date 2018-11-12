7c1c3be51a9c9b1bbd4a30c9734d64172d698e5f
huudanh2808 <huudanh2808@gmail.com>
2018-11-12 23:05:40 +0700

    [ADD] add send photo by photo url

--- src/component/MessageBlock.js
+++ src/component/MessageBlock.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import '../css/MessageBlock.css'
 
+const FROM_FRIEND_BLOCK = "message-block float-md-left row";
+const FROM_USER_BLOCK = "message-block d-flex flex-row-reverse"
+var MESSAGE_CONTENT_CONTAINER = "message-content-container"
+const isPhotoUrl = (url) => {
+    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+}
+
 const MessageBlock = ({ sendTime, message, photoUrl, from, authId }) => {
-    var className = "";
-    if (from === authId) {
-        className = "message-block d-flex flex-row-reverse";
-        
-    }
-    else {
-        className = "message-block float-md-left row";
-        
+    var rootClassName = "";
+    var messageContentContainerClassName = MESSAGE_CONTENT_CONTAINER;
+    var messageContent = <p>{message}</p>
+    rootClassName = from === authId ? FROM_USER_BLOCK : FROM_FRIEND_BLOCK;
+    if(isPhotoUrl(message)){
+        messageContent = <img className={"photo-in-message"} src={message} alt=""/>
+        messageContentContainerClassName = messageContentContainerClassName.concat(" img-block")
     }
     return (
-        <div className={className}>
+        <div className={rootClassName}>
             <img className={"chat-avt"} src={photoUrl} alt="" />
-            <div className={"message-content-container"}>
+            <div className={messageContentContainerClassName}>
                 <div className={"message-content"}>
-                    <p>{message}</p>
+                   {messageContent}
                 </div>
                 <div className={"send-time"}>
                     {sendTime}
